bcccbf250329f2b9e20c4bc14e7fb61ea3bc7e03
Vasco Santos <vasco.santos@mindera.com>
2017-01-21 11:19:32 +0000

    Bifrost service as a client of Configuration Service

--- bifrost-service/src/main/java/com/mindera/techday/bifrost/indicator/CustomHealthIndicator.java
+++ bifrost-service/src/main/java/com/mindera/techday/bifrost/indicator/CustomHealthIndicator.java
@@ -1,13 +1,23 @@
 package com.mindera.techday.bifrost.indicator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+@RefreshScope
 @Component
 public class CustomHealthIndicator implements HealthIndicator {
+
+    private String statusMessage;
+
+    public CustomHealthIndicator(@Value("${message}") String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
     @Override
     public Health health() {
-        return Health.status("Babidiboopi!").build();
+        return Health.status(statusMessage).build();
     }
 }
