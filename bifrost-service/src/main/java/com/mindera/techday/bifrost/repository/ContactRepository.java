8c1a448945f2d6faebe4d846f21c7754aaa99ce3
Vasco Santos <vasco.santos@mindera.com>
2017-01-21 11:07:05 +0000

    Bifrost Rest endpoints

--- bifrost-service/src/main/java/com/mindera/techday/bifrost/repository/ContactRepository.java
+++ bifrost-service/src/main/java/com/mindera/techday/bifrost/repository/ContactRepository.java
@@ -2,10 +2,15 @@ package com.mindera.techday.bifrost.repository;
 
 import com.mindera.techday.bifrost.domain.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Collection;
 
+@RepositoryRestResource
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     // select * from contacts where phone_number=:phoneNumber
-    Collection<Contact> findByPhoneNumber(String PhoneNumber);
+    @RestResource(path = "by-name")
+    Collection<Contact> findByPhoneNumber(@Param("phoneNumber") String PhoneNumber);
 }
