db0075d83af8056efec3685a3fecd06e0aa159e0
Vasco Santos <vasco.santos@mindera.com>
2017-01-21 11:07:46 +0000

    Bifrost management configurations

---
+++ bifrost-service/src/main/java/com/mindera/techday/bifrost/indicator/CustomHealthIndicator.java
@@ -0,0 +1,13 @@
+package com.mindera.techday.bifrost.indicator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.status("Babidiboopi!").build();
+    }
+}
